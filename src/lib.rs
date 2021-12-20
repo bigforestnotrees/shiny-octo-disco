946759a6051503e9b9cfee74d44d8c560ee755d5
Liam H <5874952+bigforestnotrees@users.noreply.github.com>
2021-12-20 16:56:13 -0500

    Added iterator

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,35 @@ fn get_regex(s: String) -> Result<Regex, Error> {
     Regex::new(&s)
 }
 
+impl<'a, T: From<&'a str>> Iterator for ReString<'a, T> {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index >= self.string.len() {
+            return None;
+        }
+        self.string = &self.string[self.index..];
+
+        let re = match get_regex(String::from(self.regex)) {
+            Ok(c) => c,
+            Err(_) => return None,
+        };
+
+        let m = re.find(self.string)?;
+
+        if m.as_str().is_empty() {
+            return None;
+        } else {
+            self.result = T::from(m.as_str());
+            self.index = m.end();
+            
+            Some(String::from(self.string))
+        }
+    }
+}
+
 impl<'a, T: From<&'a str>> ReString<'a, T> {
-    pub fn new(s: &'a str, sre: &str) -> Option<Self> {
+    pub fn new(s: &'a str, sre: &'a str) -> Option<Self> {
         let re = match get_regex(String::from(sre)) {
             Ok(c) => c,
             Err(_) => return None,
@@ -22,14 +49,16 @@ impl<'a, T: From<&'a str>> ReString<'a, T> {
             let result = T::from(m.as_str());
             let index = m.end();
             
-            Some(Self {result, index, phantom_data: PhantomData})
+            Some(Self {string: s, result, index, regex: sre, phantom_data: PhantomData})
         }
     }
 }
 
 pub struct ReString<'a, T: From<&'a str>> {
+    pub string : &'a str,
     pub result : T,
     pub index : usize,
+    pub regex : &'a str,
     phantom_data: PhantomData<&'a T>,
 }
 
--- tests/unit_tests.rs
+++ tests/unit_tests.rs
@@ -1,8 +1,28 @@
 #[cfg(test)]
 mod tests {
     #[test]
-    fn string_of_ints() {
-        let result : regex_types::ReString<&str> = regex_types::ReString::new("Hello, World!", "World").unwrap();
+    fn hello_world() {
+        let result: regex_types::ReString<&str> =
+            regex_types::ReString::new("Hello, World!", "World").unwrap();
         assert_eq!(result.result, "World");
+    }
+
+    #[test]
+    fn iterator_test() {
+        let s = "Good, good! Let's the hate flow through you. GOOD!";
+
+        for (i, result) in regex_types::ReString::<String>::new(s, "[Gg]")
+            .unwrap()
+            .into_iter()
+            .enumerate()
+        {
+            match i {
+                0 => assert_eq!(result, "ood, good! Let's the hate flow through you. GOOD!"),
+                1 => assert_eq!(result, "ood! Let's the hate flow through you. GOOD!"),
+                2 => assert_eq!(result, "h you. GOOD!"),
+                3 => assert_eq!(result, "OOD!"),
+                _ => unreachable!(),
+            };
+        }
     }
 }
